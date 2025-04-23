d806d661b04f97b7d7f45269142b5dd347970a0c
James Garza <james@trilliumcapital.ie>
2025-04-23 23:28:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,6 @@ unique_tickers = sorted(df["symbol"].dropna().unique())
 selected_ticker = st.sidebar.selectbox("Select Ticker", ["All"] + unique_tickers)
 
 df['transactionDate'] = df['transactionDate'].dt.strftime('%Y-%m-%d',)
-df['transactionDate'] = pd.to_datetime(df['transactionDate'])
 
 if not df.empty:
     default_start = df["transactionDate"].min().date()
