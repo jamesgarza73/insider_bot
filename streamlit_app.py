c3240e3958efb6a9b45701a5616bfbf95adc5478
James Garza <james@trilliumcapital.ie>
2025-04-23 18:07:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ st.title("📊 Congressional Trading Dashboard")
 @st.cache_data(ttl=60)
 def load_trades():
     try:
-        return pd.read_csv(SAVE_PATH, parse_dates=["TransactionDate", "disclosureDate"], low_memory=False)
+        return pd.read_csv(SAVE_PATH, parse_dates=["transactionDate", "disclosureDate"], low_memory=False)
     except (pd.errors.EmptyDataError, ValueError):
-        return pd.DataFrame(columns=["disclosureDate", "Ticker", "TransactionDate", "firstName", "lastName", "type", "Amount"])
+        return pd.DataFrame(columns=["disclosureDate", "Ticker", "transactionDate", "firstName", "lastName", "type", "Amount"])
 
 
 df = load_trades()
